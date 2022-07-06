1e623e0c84fa88c26fb220669674f210c0c9fb66
Kqpa <kqpa@pm.me>
2022-07-06 09:45:01 +0300

    ok

--- misc/replace_ip.sh
+++ misc/replace_ip.sh
@@ -2,10 +2,10 @@
 
 FILE_NAME='./../data.json'
 NEW_IP="$1"
-COMMIT_MESSAGE='updated the server IP address on `server.kqpa.me`'
+COMMIT_MESSAGE='Updated the server IP address on `server.kqpa.me`'
 
 if [ $# -eq 0 ]; then
-	echo "usage: $0 <ip>" && exit 1
+	echo "usage: $0 <IP>" && exit 1
 fi
 
 : > $FILE_NAME
@@ -16,8 +16,8 @@ cat <<EOT >> $FILE_NAME
 }
 EOT
 
-git add . && git pull && git commit -m "$COMMIT_MESSAGE" && git push
+git add -A :/ && git pull && git commit -m "$COMMIT_MESSAGE" && git push
 
-echo "$0: successfully replaced the ip in '$FILE_NAME'"
+echo "$0: successfully replaced the IP in '$FILE_NAME' with '$NEW_IP'"
 
 exit 0
