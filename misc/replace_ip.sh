fc101b298cf0435c97611251bbb15b2309cf3c9f
Kqpa <kqpa@pm.me>
2022-07-06 11:20:26 +0300

    ok

--- misc/replace_ip.sh
+++ misc/replace_ip.sh
@@ -3,14 +3,14 @@
 FILE_NAME='./../data.json'
 
 NEW_IP="$1"
-NEW_COMMIT_MESSAGE="Updated the server IP address on \`server.kqpa.me\` to \`$NEW_IP\`"
+NEW_COMMIT_MESSAGE="Updated the server IP address to \`$NEW_IP\`"
 
 DOWN_MESSAGE="Server Down"
-DOWN_COMMIT_MESSAGE='Took the server down on `server.kqpa.me`'
+DOWN_COMMIT_MESSAGE="Took the server down on \`server.kqpa.me\`"
 
 if [ $# -eq 0 ]; then
 	echo "usage: $0 <IP> # Updates server IP" && \
-	echo "usage: $0 -s # Stops the server" && \
+	echo "usage: $0 -s   # Stops the server" && \
 	exit 1
 fi
 
